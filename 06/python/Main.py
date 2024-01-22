2c9ad0b2520e5c67ed889c2848e66b32de2fdfa7
Ari Barzilai <aribarzilai@gmail.com>
2024-01-22 17:52:58 +0200

    Added folder-assembling functionality

--- 06/python/Main.py
+++ 06/python/Main.py
@@ -6,22 +6,33 @@ import sys
 
 
 def main():
-    """Main function for the program.\n
-    Takes in a file name as an argument and outputs a .hack file with the same name as the input file.
+    if len(sys.argv) < 2:
+        raise ValueError("Error! Enter a path to a file or a directory as an argument.")
+    
+    path = os.path.normpath(sys.argv[1])
+    
+    if os.path.isdir(path):
+        for file in os.listdir(path):
+            if file.endswith(".asm"):
+                assembleFile(os.path.join(path, file))
+    else:
+        assembleFile(path)
+
+def assembleFile(filePath):
+    """Takes in a file name as an argument and outputs a .hack file with the same name as the input file.
 
     Raises:
         ValueError: If no argument is given.
     """
-    if len(sys.argv) < 2:
-        raise ValueError("Error! Enter an argument")
     
-    directory, filename = os.path.split(os.path.normpath(sys.argv[1]))
+    directory, filename = os.path.split(filePath)
     directory = os.path.join(os.path.dirname(__file__), directory)
     parser = ps.Parser(os.path.join(directory, filename))
     outputFileName = f"{os.path.splitext(filename)[0]}.hack"
     outputFile = os.path.join(directory, outputFileName)
     
     with open(outputFile, 'w') as file:
+            print(f"Assembling {filename}...")
             while parser.has_more_commands():
                     parser.advance()
                     output = "111"
@@ -35,11 +46,12 @@ def main():
                         file.write(output + "\n")
                     except:
                         pass
-                    debug(parser, output)
+                    #debug(parser, output)
+    print(f"Successfully assembled {filename} into {outputFileName}.")
 
 
 def debug(parser, output):
-    template = "{:<5} | {:<8} | dest {:<6} comp {:<6} jump {:<6} symbol {:<5} code {:<16} | {:<7} {:3} {:3}"
+    template = "{:<5} | {:<8} | dest {:<6} | comp {:<6} | jump {:<6} | symbol {:<5} | bin {:<16} | {:<7} {:3} {:3}"
     print(template.format(
         parser.current_line if parser.current_line is not None else "None",
         parser.command_type if parser.command_type is not None else "None",
