ef4e81a0e083adef124db3b8c322f842e5f9479f
Ari Barzilai <aribarzilai@gmail.com>
2024-01-29 14:47:56 +0200

    Slight refactoring and documentation

--- 06/python/SymbolTable.py
+++ 06/python/SymbolTable.py
@@ -1,8 +1,5 @@
 class SymbolTable(object):
-    
-    symbol_table = {}
-    next_line_index = 0 # the ROM address of the next line of code to be scanned during initialization
-    next_var_index = 16 # the RAM address of the next variable to be stored in the symbol table, starting from index 16
+    """A class that stores the symbols in a Hack language program and their corresponding memory addresses."""
     
     def __init__(self, parser):
         # Creates and initializes a SymbolTable with the predefined symbols defined in the Hack language.
@@ -10,10 +7,9 @@ class SymbolTable(object):
                'R0':0, 'R1':1, 'R2':2, 'R3':3, 'R4':4, 'R5':5, 'R6':6, 'R7':7,
                'R8':8, 'R9':9, 'R10':10, 'R11':11, 'R12':12, 'R13':13, 'R14':14, 'R15':15,
                'SCREEN':16384, 'KBD':24576}
-        self.next_line_index = 0
-        self.next_var_index = 16
-        self.add_labels_from_file(parser)
-        
+        self.next_line_index = 0 # the ROM address of the next line of code to be scanned during initialization
+        self.next_var_index = 16 # the RAM address of the next variable to be stored in the symbol table, starting from index 16
+        self.add_labels_from_file(parser)      
     
     # Adds the pair (symbol,address) to the table.      
     def add_entry(self, symbol, address):
