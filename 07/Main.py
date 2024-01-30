1db2c43f37287c719511dae177cbacef1b022518
Ari Barzilai <aribarzilai@gmail.com>
2024-01-30 08:06:42 +0200

    standardized double quotes

--- 07/Main.py
+++ 07/Main.py
@@ -6,26 +6,26 @@ import sys
 
 def main():
     if len(sys.argv) < 2:
-        raise ValueError("Error! Enter a path to a file or a directory as an argument.")
+        raise ValueError('Error! Enter a path to a file or a directory as an argument.')
     
     path = os.path.normpath(sys.argv[1])
     path = os.path.abspath(path)  
     parent_dir, file_name = os.path.split(path)              
-    output_fname = f"{os.path.splitext(file_name)[0]}.asm"
+    output_fname = f'{os.path.splitext(file_name)[0]}.asm'
     output_path = os.path.join(path, output_fname) if os.path.isdir(path) else os.path.join(parent_dir, output_fname)
     
     code_writer = Code.CodeWriter(output_path)        
     if os.path.isdir(path):     
-        print("translating the files in {}".format(path))   
+        print('translating the files in {}'.format(path))   
         for file in os.listdir(path):
-            if file.endswith(".vm"):
+            if file.endswith('.vm'):
                 input_file = os.path.join(path, file)
                 translate_file(input_file, code_writer)
     else:
         translate_file(path, code_writer)  
     code_writer.close()
     
-    print("Done! File is located at {}".format(output_path))  
+    print('Done! File is located at {}'.format(output_path))  
         
 def translate_file(file_path, code_writer : Code):
     """Takes in a normalized file name as an argument and outputs to a .asm file
@@ -35,7 +35,7 @@ def translate_file(file_path, code_writer : Code):
 
     parser = ps.Parser(file_path)
     fname = os.path.basename(file_path)
-    print(f"Translating {fname}...")
+    print(f'Translating {fname}...')
     while parser.has_more_commands():
             parser.advance()
             if parser.command_type == C.PUSH:
